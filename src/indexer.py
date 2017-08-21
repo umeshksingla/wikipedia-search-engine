aa0115e2222bb69c082f27662d83497e63ea2252
Umesh Singla <umeshksingla@gmail.com>
2017-08-21 23:18:12 +0530

    Add Indexer job and Add write-to-file function
    Also, correct few bugs in PageIndex class

--- src/indexer.py
+++ src/indexer.py
@@ -9,129 +9,146 @@
 	Python Version: 2.7
 '''
 
+import contentHandler as content_p
+import textHighLevelParser as retrieve_p
+
+import codecs
 import sys
-import xml.sax
+import os
 
-from page import Page
+from multiprocessing import Process, JoinableQueue
 
-class WikiContentHandler(xml.sax.ContentHandler):
-	"""
-	These methods are called by the parser on the appropriate events in the
-	input document.
-	"""
-	def __init__(self, pagesQueue):
-
-		# queue to process pages
-		self.pagesQueue = pagesQueue
-
-		self.locator = None
-
-		# current page information
-		self.currentPage = None
-		# content of the current (desired) elements
-		self.content = ""
-
-		self.totalPages = 0
-
-		# xml elements occuring in the order
-		self.elements = []
-
-	def setDocumentLocator(self, locator):
-		"""
-		Called by the parser to give the application a locator for locating
-		the origin of document events.
-
-		Provies a way to associate a SAX event with a page location.
-		"""
-		self.locator = locator
-
-	def startDocument(self):
-		pass
-
-	def endDocument(self):
-		"""
-		at the end of docoument, send the pages to pre-process
-		"""
-		# add a keyword 'stop' at the end of a queue
-		self.pagesQueue.put("stop")
-		self.pagesQueue.close()
-
-		# join the parent process
-		self.pagesQueue.join()
-
-	def startElement(self, name, attrs):
-		"""
-		when the event is "start" in the xml-tree
-		"""
-		# need to process its contents
-		if name == "page":
-			# create a nice doucment out of it
-			self.currentPage = Page(self.locator.getLineNumber())
-			self.totalPages = self.totalPages + 1
-
-		elements.append(name)
-		self.content = ""
-
-	def endElement(self, name):
-		"""
-		when the event is "end" in the xml-tree
-		"""
-		lastTag = elements[-1]
-		if name == "page":
-			# add it to the queue to process
-			self.pagesQueue.put(self.currentDoc)
-			# nullify the current document
-			self.currentPage = None
-		elif name == "text":
-			self.currentPage.setText(self.data)
-		elif name == "title":
-			self.currentPage.setTitle(self.data)
-		elif name == "id" and lastTag == "page":
-			# use the id of the page as the currentPage's id
-			self.currentPage.setId(self.data)
-
-		# remove the current element from path
-		self.currentPath.pop()
-
-	def characters(self, content):
-		"""
-		the valid tags' content - id, title, text here
-		"""
-		lastTag = elements[-1]
-		if lastTag == "title" or lastTag == "id" or lastTag == "text":
-			self.content += content
-
-
-def parser(xmlFile):
+class PageIndex:
 	"""
 	"""
-	# implement own content handler
-	contentHandler = WikiContentHandler()
-	
-	# get a SAX XMLReader object
-	parser = xml.sax.make_parser()
+	def __init__(self, outputFile):
 
-	# use above content handler instead of default
-	parser.setContentHandler(contentHandler)
+		self.outputFile = outputFile
+		self.fileNo = 0
 
-	# create a SAX parser and use it to parse a document
-	parser.parse(xmlFile)
+		self.index = {}
+		self.pages = 0
+
+	def getfile(self, n=None):
+		"""
+		"""
+		if n is None:
+			n = self.fileNo
+			self.fileNo += 1
+
+		return self.outputFile + '/index' + str(n)
+
+	def indexPage(self, page):
+		"""
+		count of a word in each document, separately for title, body,
+		headings, references, links etc.
+		"""
+
+		pageId = page["pageId"]
+		fields = page["fields"]
+
+		for f in fields:
+			for word in fields[f]:
+				id = word + "_" + f
+				if id not in self.index:
+					self.index[id] = {}
+
+				if pageId not in self.index[id]:
+					self.index[id][pageId] = 0
+
+				self.index[id][pageId] += 1
+
+		self.pages += 1
+
+		# when so many files created
+		if self.pages >= 500000:
+			self.writeToFile(self.getfile())
+			self.index = {}
+			self.pages = 0
+
+	def writeToFile(self, fname):
+		"""
+		"""
+		d = codecs.open(fname, "w", "utf-8")
+
+		word_list = self.index.keys()
+		word_list.sort()
+
+		for w in word_list:
+
+			# get the pages id in sorted order or linear merging
+			pages = sorted(self.index[w].keys(), key=lambda x: int(x))
+
+			c = str(len(self.index[w]))
+
+			d.write(w + "_" + c + ":")
+			for p in pages:
+				c = str(self.index[w][p])
+				d.write(str(p) + "_" + c + ",")
+			d.write('\n')
+			del self.index[w]
+
+		d.close()
+
+	def finishIndexing(self):
+		"""
+		"""
+		fm = []
+		c = 0
+
+		self.writeToFile(self.getfile())
+
+		self.index = {}
+		self.pages = 0
+
+
+def jobs(pages_retrieve_sequence, outputFile):
+	"""
+	"""
+
+	index = PageIndex(outputFile)
+
+	if not os.path.exists(outputFile):
+		os.makedirs(outputFile)
+
+	while True:
+		page = pages_retrieve_sequence.get()
+		if page == "finished":
+			pages_retrieve_sequence.task_done()
+			break
+		index.indexPage(page)
+		pages_retrieve_sequence.task_done()
+
+	index.finishIndexing()
 
 
 def index(xmlFile, outputFile):
 	"""
-	Steps:
-	1. parse
-	2. index
 	"""
-	parser(xmlFile)
-	pass
+	if not os.path.exists(outputFile):
+		os.makedirs(outputFile)
+
+	pages_parse_sequence = JoinableQueue(15)
+	pages_retrieve_sequence = JoinableQueue(10000)
+
+	content_pr = Process(target=content_p.parser, args=(xmlFile, pages_parse_sequence))
+	content_pr.start()
+
+	retrieve_pr = Process(target=retrieve_p.parser, args=(pages_parse_sequence, pages_retrieve_sequence))
+	retrieve_pr.start()
+
+	index_pr = Process(target=jobs, args=(pages_retrieve_sequence, outputFile))
+	index_pr.start()
+
+	content_pr.join()
+	retrieve_pr.join()
+	index_pr.join()
 
 
 if __name__ == '__main__':
 	if len(sys.argv) < 3:
 		print """ USAGE: bash index.sh <path-to-wiki-dump> 
-			<path-to-invertedindex (outputfile)> """
+			<path-to-invertedindex (output directory)> """
 		sys.exit(1)
 
 	index(sys.argv[1], sys.argv[2])
