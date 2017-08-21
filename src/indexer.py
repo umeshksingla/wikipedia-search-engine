11f6e5f740e9f5bf127aad932ec06d443c0d3b25
Umesh Singla <umeshksingla@gmail.com>
2017-08-21 15:57:45 +0530

    Finish custom content handler

--- src/indexer.py
+++ src/indexer.py
@@ -12,6 +12,96 @@
 import sys
 import xml.sax
 
+from page import Page
+
+class WikiContentHandler(xml.sax.ContentHandler):
+	"""
+	These methods are called by the parser on the appropriate events in the
+	input document.
+	"""
+	def __init__(self, pagesQueue):
+
+		# queue to process pages
+		self.pagesQueue = pagesQueue
+
+		self.locator = None
+
+		# current page information
+		self.currentPage = None
+		# content of the current (desired) elements
+		self.content = ""
+
+		self.totalPages = 0
+
+		# xml elements occuring in the order
+		self.elements = []
+
+	def setDocumentLocator(self, locator):
+		"""
+		Called by the parser to give the application a locator for locating
+		the origin of document events.
+
+		Provies a way to associate a SAX event with a page location.
+		"""
+		self.locator = locator
+
+	def startDocument(self):
+		pass
+
+	def endDocument(self):
+		"""
+		at the end of docoument, send the pages to pre-process
+		"""
+		# add a keyword 'stop' at the end of a queue
+		self.pagesQueue.put("stop")
+		self.pagesQueue.close()
+
+		# join the parent process
+		self.pagesQueue.join()
+
+	def startElement(self, name, attrs):
+		"""
+		when the event is "start" in the xml-tree
+		"""
+		# need to process its contents
+		if name == "page":
+			# create a nice doucment out of it
+			self.currentPage = Page(self.locator.getLineNumber())
+			self.totalPages = self.totalPages + 1
+
+		elements.append(name)
+		self.content = ""
+
+	def endElement(self, name):
+		"""
+		when the event is "end" in the xml-tree
+		"""
+		lastTag = elements[-1]
+		if name == "page":
+			# add it to the queue to process
+			self.pagesQueue.put(self.currentDoc)
+			# nullify the current document
+			self.currentPage = None
+		elif name == "text":
+			self.currentPage.setText(self.data)
+		elif name == "title":
+			self.currentPage.setTitle(self.data)
+		elif name == "id" and lastTag == "page":
+			# use the id of the page as the currentPage's id
+			self.currentPage.setId(self.data)
+
+		# remove the current element from path
+		self.currentPath.pop()
+
+	def characters(self, content):
+		"""
+		the valid tags' content - id, title, text here
+		"""
+		lastTag = elements[-1]
+		if lastTag == "title" or lastTag == "id" or lastTag == "text":
+			self.content += content
+
+
 def parser(xmlFile):
 	"""
 	"""
@@ -36,6 +126,7 @@ def index(xmlFile, outputFile):
 	"""
 	parser(xmlFile)
 	pass
+
 
 if __name__ == '__main__':
 	if len(sys.argv) < 3:
