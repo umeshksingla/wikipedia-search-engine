e2d75551cb2237bb091b4611960a81de3d235034
Umesh Singla <umeshksingla@gmail.com>
2017-09-11 05:26:45 +0530

    Add util to serve search on terminal

---
+++ src/search/searcher.py
@@ -0,0 +1,28 @@
+#!/usr/local/bin/python
+# coding: utf8
+
+'''
+	File name: searcher.py
+	File description: Interface for search
+	Author: Umesh Singla
+	Date created: Sep 11, 2017
+	Python Version: 2.7
+'''
+
+import search
+
+def serve(outputFile):
+	"""
+	Serve the interface for searching a given query
+	"""
+	search.initialize(outputFile)
+
+	while True:
+		query = raw_input()
+		terms = query.strip().lower().split()
+		if terms:
+			result = search.search(terms)
+			print result
+			print '\n'
+		else:
+			print "Search field can not be empty\n"
